5c3fc63146e7044e4d4f6e567663b1766ec84ed7
MinHyeok(James) <nogoso3@gmail.com>
2023-09-11 11:49:40 +0900

    choreSignInForm 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
 'use client';
+
 // import ProductCard from './components/organisms/ProductCard';
-import ProductForm, { IProductFormData } from './components/organisms/ProductForm';
 import Header from '@components/organisms/Header';
+import ProductForm, { IProductFormData } from '@components/organisms/ProductForm';
+import SigninForm from '@components/organisms/SigninForm';
+
 export default function Home() {
   const handleProductSave = (data: IProductFormData) => {
     console.info(data);
+  };
+
+  const handleSignIn = (username: string, password: string) => {
+    console.log(username, password);
   };
   return (
     <>
       <Header />
       {/* <ProductCard title="빠른 신발" imageUrl="/images/1.jpg" price={20000} /> */}
       <ProductForm onProductSave={handleProductSave}></ProductForm>
+      <SigninForm onSignin={handleSignIn} />
     </>
   );
 }
