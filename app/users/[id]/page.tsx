443f10cd41b3ab10339a88134ce951b93e8a40cc
MinHyeok(James) <nogoso3@gmail.com>
2023-09-25 00:47:29 +0900

    롤백

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,23 +1,22 @@
 'use client';
 
+// ssg 생성되지 않은 페이지 접근시 작업 여부를 제어.
+export const dynamicParams = false;
+
 import { lazy, Suspense } from 'react';
 import Box from '@/app/components/layout/Box';
 import Flex from '@/app/components/layout/Flex';
-import UserProfileContainer from '@/app/containers/UserProfileContainer';
 import getAllProducts from '@/app/services/get-all-products';
-// import getUser from '@/app/services/users/getUser';
-import useUser from '@/app/services/users/use-user';
+import getUser from '@/app/services/users/getUser';
 import type { Product, User, ApiContext } from '@/app/types';
-// import { isNotNull } from '@/app/utils/utils';
 import getAllUser from '@services/users/get-all-users';
 
 // React.lazy 컴포넌트를 지연로딩한다.
 // Suspense하위 컴포넌트로 사용되어야함.
 const ProductView = lazy(() => import('./product'));
-const url = process.env.API_BASE_URL || 'http://127.0.0.1:5000';
+const UserProfileContainer = lazy(() => import('@/app/containers/UserProfileContainer'));
 
-// ssg 생성되지 않은 페이지 접근시 작업 여부를 제어.
-export const dynamicParams = false;
+const url = process.env.API_BASE_URL || 'http://127.0.0.1:5000';
 
 interface IUserParams {
   id: string;
@@ -40,30 +39,29 @@ function getProductList(id: string) {
   return getAllProducts({ apiRootUrl: url }, id); // throw Promise or Product[]
 }
 
-export default function Page({ params }: { params: IUserParams }) {
-  // 훅은 컴포넌트 내 최상위에서 사용해야함.
-  const { user } = useUser({ apiRootUrl: url }, { id: params.id });
+function getUserInfo(id: string) {
+  return getUser({ apiRootUrl: url }, id);
+}
 
-  if (user !== undefined) {
-    return (
-      <Flex
-        paddingtop="16px"
-        paddingbottom="16px"
-        paddingleft={{ base: '16px', md: '4px' }}
-        paddingright={{ base: '16px', md: '4px' }}
-        justifycontent="center"
-      >
-        <Box width="1180px">
+export default function Page({ params }: { params: IUserParams }) {
+  return (
+    <Flex
+      paddingtop="16px"
+      paddingbottom="16px"
+      paddingleft={{ base: '16px', md: '4px' }}
+      paddingright={{ base: '16px', md: '4px' }}
+      justifycontent="center"
+    >
+      <Box width="1180px">
+        <Suspense fallback={<p>데이터 가져온다굿...</p>}>
           <Box marginbottom="8px">
-            <UserProfileContainer user={user} />
+            <UserProfileContainer user={getUserInfo(params.id)} />
           </Box>
-          <Suspense fallback={<p>데이터 가져온다굿...</p>}>
-            <Box marginbottom="8px">
-              <ProductView products={getProductList(params.id)} />
-            </Box>
-          </Suspense>
-        </Box>
-      </Flex>
-    );
-  }
+          <Box marginbottom="8px">
+            <ProductView products={getProductList(params.id)} />
+          </Box>
+        </Suspense>
+      </Box>
+    </Flex>
+  );
 }
