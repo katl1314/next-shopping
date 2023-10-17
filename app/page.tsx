567595169bfec829f987ada5303a7e54a8d05400
katl1314 <nogoso3@gmail.com>
2023-10-18 02:13:43 +0900

    스타일 수정

--- app/page.tsx
+++ app/page.tsx
@@ -81,15 +81,15 @@ const HomePage: NextPage<IHomePageProps> = () => {
         justifycontent="center"
         flexdirection="column"
       >
-        <Box width="100%">
+        <Box width="90%">
           <Text variant="large">의류</Text>
           {renderProductCardCarousel(clothesProducts.data)}
         </Box>
-        <Box width="100%" margin="0 auto">
+        <Box width="90%" margin="0 auto">
           <Text variant="large">도서</Text>
           {renderProductCardCarousel(bookProducts.data)}
         </Box>
-        <Box width="100%" margin="0 auto">
+        <Box width="90%" margin="0 auto">
           <Text variant="large">신발</Text>
           {renderProductCardCarousel(shoesProducts.data)}
         </Box>
